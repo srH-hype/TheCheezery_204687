9c6ccec0e8ac984ebde2866d593cd8a231a7caee
srH-hype <jejeje300@gmail.com>
2022-02-23 13:52:09 -0700

    parte 2

--- TheCheezeys/app/src/main/java/imperial/heriberto/thecheezeys/MenuActivity.kt
+++ TheCheezeys/app/src/main/java/imperial/heriberto/thecheezeys/MenuActivity.kt
@@ -1,11 +1,40 @@
 package imperial.heriberto.thecheezeys
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnCold: Button = findViewById(R.id.coldDrinksButton) as Button
+        var btnHot: Button = findViewById(R.id.hotDrinksButton) as Button
+        var btnSweet: Button = findViewById(R.id.sweetsButton) as Button
+        var btnSalt: Button = findViewById(R.id.saltiesButton) as Button
+
+        btnCold.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo","coldDrinks")
+            startActivity(intent)
+        }
+        btnHot.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo","hotDrinks")
+            startActivity(intent)
+        }
+        btnSweet.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo","sweets")
+            startActivity(intent)
+        }
+        btnSalt.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo","salties")
+            startActivity(intent)
+        }
+
     }
 }
---
+++ TheCheezeys/app/src/main/java/imperial/heriberto/thecheezeys/Product.kt
@@ -0,0 +1,8 @@
+package imperial.heriberto.thecheezeys
+
+data class Product(
+    var name: String,
+    var image: Int,
+    var description: String,
+    var price: Double,
+)
